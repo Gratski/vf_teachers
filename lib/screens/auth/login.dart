dbd6560904e3c291d231e7954a383768e4958938
João Rodrigues <rodrigues.at.work@gmail.com>
2020-04-03 22:17:45 +0100

    added queues

--- lib/screens/auth/login.dart
+++ lib/screens/auth/login.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:teachers/components/buttons/VFFlatButtonPrimary.dart';
 import 'package:teachers/components/inputs/VFTextInput.dart';
-import 'package:teachers/components/text/VFTitle.dart';
 import 'package:teachers/screens/auth/PasswordRecovery.dart';
+import 'package:dart_amqp/dart_amqp.dart';
 
 /**
  * Login Sccreen
@@ -11,6 +11,36 @@ class LoginScreen extends StatelessWidget {
   final _formKey = GlobalKey<FormState>();
   TextEditingController _emailInputController = new TextEditingController();
   TextEditingController _pwdInputController = new TextEditingController();
+
+  LoginScreen() {
+    _init();
+  }
+
+  _init() async {
+    try {
+      ConnectionSettings settings = new ConnectionSettings(
+          host : "whale.rmq.cloudamqp.com",
+          virtualHost: "uzmmozvh",
+          authProvider: new AmqPlainAuthenticator("uzmmozvh","hekpyaJiCK61YIsYRqBLy2VJAvH3K41c")
+      );
+      Client client = new Client(settings: settings);
+      client
+          .channel()
+          .then((Channel channel) => channel.queue("hello", durable: true))
+          .then((Queue queue) => queue.consume())
+          .then((Consumer consumer) => consumer.listen((AmqpMessage message) {
+        print(" [x] Received string: ${message.payloadAsJson["name"]}");
+      }
+      ));
+
+      client
+          .channel()
+          .then((Channel channel) => channel.queue("hello", durable: true))
+          .then((Queue queue) => queue.publish("{\"name\":\"Joao juntou-se à aula! :)\"}"));
+    } catch(e) {
+      print("Ocurreu um erro ao connector a karafka");
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
